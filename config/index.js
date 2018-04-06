1be5c8daffbadf8770b4f0ec80d39f18280c4f12
Valère Pique <valere@1000mercis.com>
2018-04-07 01:24:27 +0200

    Build 3.0.5 : Fix index.html

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     index: path.resolve(__dirname, '../index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'docs',
-    assetsPublicPath: '/vue-multi-select/dist',
+    assetsPublicPath: '/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
